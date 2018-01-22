5ac1b32318e9e42817853f60944d1fcd15171554
keshav kummari <keshav.kummari@gmail.com>
2018-01-22 06:33:03 +0530

    keywords

---
+++ keywords.py
@@ -0,0 +1,29 @@
+# 1. Python Keywords:
+
+Keywords are the reserved words in Python.
+
+We cannot use a keyword as variable name, function name or any other identifier. 
+
+They are used to define the syntax and structure of the Python language.
+
+In Python, keywords are case sensitive.
+
+There are 33 keywords in Python 3.3. 
+
+All the keywords except True, False and None are in lowercase and they must be written as it is. 
+
+The list of all the keywords are given below.
+
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+False               def                 if                  raise
+None                del                 import              return
+True                elif                in                  try
+and                 else                is                  while
+as                  except              lambda              with
+assert              finally             nonlocal            yield
+break               for                 not
+class               from                or
+continue            global              pass
+
+
