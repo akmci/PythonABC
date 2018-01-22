7f578fdcb3dd0c6a4583008c9af1aab93624bbef
keshav kummari <keshav.kummari@gmail.com>
2018-01-22 07:02:32 +0530

    print

---
+++ printexamples.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+highScoreValue = 1          # Camel-Case Style Variable
+a = 1
+A = 2
+Red_007_hat = 100   
+_007= 200
+_007_PyCharm = 200
+
+print ("Lets access Camel-Case Style Variable ",highScoreValue, end="##")
+
+print (a,"Access a lower case variable")
+
+print ("Access a upper case ",A,"variable")
+
+print ("Lets use combination of all : ",Red_007_hat)
+
+print (_007)
+
+print ('UnderScore Digits and Alphabets: ',_007_PyCharm)
